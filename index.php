fc6284f531f5c0895c87072bec72f6fc25ad6bd9
TiagodeMattos <Tiagodemattos2@gmail.com>
2025-10-28 21:55:18 -0300

    Update index.php

--- index.php
+++ index.php
@@ -38,5 +38,36 @@ echo "<br>";
 $valor1 = 50;
 $valor2 = 2;
 $restoDivisao = $valor1 % $valor2;
-echo "subtracao é: " . $restoDivisaos;
+echo "subtracao é: " . $restoDivisao;
 echo "<br>";
+
+// Converter graus Fahrenheite para Celcius. 95F -> ?F (35) 
+
+$fahrenheite = 95;
+$grausFahrenheite = $fahrenheite - 32;
+$celcius = $grausFahrenheite / 1.8; 
+
+echo "A conversão de fahrenheite $fahrenheite para celcius é $celcius <br>"; 
+
+$kelvin = $celcius + 273.15; 
+
+echo "A conversão de $celcius para kelvin é de $kelvin <br>;"; 
+
+$celcius = $kelvin - 273.15;
+
+echo "A conversão de $kelvin para celcius é de $celcius <br>";
+
+$tenhoDinheiro = true;
+$tenhoVontadeTomarCafe = true;
+
+if ($tenhoDinheiro && $tenhoVontadeTomarCafe)
+{
+    echo "vou tomar café<br>";
+} else if ($tenhoDinheiro && !$tenhoVontadeTomarCafe) {
+    echo "vou comprar chocolate<br>";
+} else {
+    echo "nao tenho vontade de nada<br>";
+}
+
+$soma = 10 + 10;
+
