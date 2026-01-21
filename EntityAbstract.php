9e67df9287fefc5ca53a8dc1c62982963e95706d
TiagodeMattos <Tiagodemattos2@gmail.com>
2026-01-20 21:48:50 -0300

    Create EntityAbstract.php

---
+++ EntityAbstract.php
@@ -0,0 +1,29 @@
+<?php
+
+//CRUD - Create, Read, Update e Delete.
+
+interface EntityInterface {
+    public function criar();
+    public function obter($id);
+    public function obterTodos($filtros = "");
+    public function atualizar($id);
+    public function deletar($id);
+}
+
+// Model || Entity, Representa a tabela do banco na programacao.
+abstract class EntityAbstract implements EntityInterface {
+     public $id;
+     public $usuarioAlteracao;
+     public $criadoEm;
+     public $atualizadoEm;
+
+     public function criar() {
+
+
+     }
+     public function obter($id) {
+
+     }
+     public function obterTodos($filtros)
+     
+}
